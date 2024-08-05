c262cbb4e253ea385a49c815588a9e402e447b58
selestrel07 <selestrel07@gmail.com>
2024-08-05 22:49:23 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  if (!matrix) {
+    return result;
+  }
+  for(let i = 0; i < matrix.length; i++) {
+    result.push(i % 2 === 1 ? [...[...matrix[i]].reverse()] : [...matrix[i]]);
+  }
+  return result.flat();
 }
